36deeb1cf2915f9cfa945d79c98b7ac139a9ed19
YoannPaquereau <yoann.paquereau@gmail.com>
2019-02-16 14:26:59 +0100

    + accents

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
   <body>
     <p>
       <?php
-        if (!isset($_SESSION['user'])) echo "Vous n'êtes pas connecté<br/><a href=\"login.php\">Connexion</a>";
-        else echo 'Bonjour '.$_SESSION['user'].'<br/>Dernière connexion : '.$_SESSION['last_connection'].'<br/><a href="logout.php">D&eacute;connexion</a>';
+        if (!isset($_SESSION['user'])) echo "Vous n'&ecirc;tes pas connect&eacute;<br/><a href=\"login.php\">Connexion</a>";
+        else echo 'Bonjour '.$_SESSION['user'].'<br/>Derni&egrave;re connexion : '.$_SESSION['last_connection'].'<br/><a href="logout.php">D&eacute;connexion</a>';
       ?>
     </p>
 
