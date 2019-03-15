8c738ffd0100147ecfb169b6caf78db78823ad1e
YoannPaquereau <yoann.paquereau@gmail.com>
2019-03-15 23:38:41 +0100

    Ajout publication annonce
    Manque l'enregistrement des images

--- pub_annonces.php
+++ pub_annonces.php
@@ -7,82 +7,100 @@
   <?php
     require "header.php"
   ?>
-    <title> les annonces </title>
+    <title>Annonces</title>
   </head>
 
 <body>
+
   <?php
   if (isset($_FILES["fichier0"]))
-
   {
     include "register_login/myparam.inc.php";
-    try{
 
-    $bdd = new PDO('mysql:host='.MYHOST.';dbname='.MYBASE.';charset=utf8',MYUSER,MYPASS);
-  }
-  catch(Exception $e)
-  {
-    die('erreur : '.$e->getmessage());
-  }
-  $req= $bdd->prepare("INSERT INTO annonces(type,ville,prix,date_publication,auteur)
-                           VALUES(:type,:ville,:prix,NOW(),:auteur) ");
-
-
-
-  // On exécute la requête avec nos valeurs
-  $req->execute(array(
- 'type' => $_POST["type"],
- 'ville' => $_POST["ville"],
- 'prix' => $_POST["prix"],
- 'auteur' => $_SESSION["user"]
-));
-
-  echo $_SESSION['user'].' vous avez ajoutez avec succès une publication de  type '.$_POST['type'].', a  '.$_POST['ville'].' au prix  '.$_POST['prix'].' <br>';
-  $maxtaille = 10000000;
-  $tabextension=array("png","jpeg","jpg");
-  for($i=0;$i<$_POST['nombre_images'];$i++)
-
-{
-
-  if   ($_FILES['fichier'.$i]['size'] > $maxtaille)
-       echo 'votre fichier '.$_FILES['fichier'.$i]['name'].' d&eacute;passe la taille maximale.<br>';
-  else{
-  $ext = strtolower(  substr(  strrchr($_FILES['fichier'.$i]['name'], '.')  ,1)  );
-     if (!in_array($ext,$tabextension) ) echo "fichier non pris encharge Extension incorrecte";
-     else {
-      $req= $bdd->prepare("SELECT id_annonce FROM annonces WHERE type=:type AND ville=:ville AND prix=:prix AND auteur=:auteur");
-      $req->execute(array(
-     'type' => $_POST["type"],
-     'ville' => $_POST["ville"],
-     'prix' => $_POST["prix"],
-     'auteur' => $_SESSION["user"]
- ));
- $donnees=$req->fetch();
- $id=$donnees["id_annonce"];
-
-
- $req->CloseCursor();
-    $req=$bdd->prepare("INSERT INTO image(nom_image,annonce)VALUES(:nom_image,:annonce)");
-    $req->execute(array(
-      'nom_image' => $_FILES['fichier'.$i]['name'],
-      'annonce' => $id
-    ));
-     }
-
+    try
+    {
+      $bdd = new PDO('mysql:host='.MYHOST.';dbname='.MYBASE.';charset=utf8',MYUSER,MYPASS);
+    }
+    catch(Exception $e)
+    {
+      die('erreur : '.$e->getmessage());
     }
 
- }
-
-}
-
+    $req= $bdd->prepare("INSERT INTO annonces(type,ville,prix,date_publication,auteur)
+                         VALUES(:type,:ville,:prix,NOW(),:auteur)");
 
 
+    // On exécute la requête avec nos valeurs
+    $req->execute(array(
+      'type' => $_POST["type"],
+      'ville' => $_POST["ville"],
+      'prix' => $_POST["prix"],
+      'auteur' => $_SESSION["user"]
+    ));
 
+    //echo $_SESSION['user'].' vous avez ajoutez avec succès une publication de  type '.$_POST['type'].', a  '.$_POST['ville'].' au prix  '.$_POST['prix'].' <br>';
 
+    // On déclare une variable contenant la taille maximale acceptée pour une image (en octets)
+    $maxtaille = 10000000;
 
+    // Idem pour les extensions mais sous forme de tableau
+    $tabextension=array("png","jpeg","jpg");
 
+    // On va traiter toutes les images envoyées par l'utilisateur
+    for($i=0;$i<$_POST['nombre_images'];$i++)
+    {
 
+      if ($_FILES['fichier'.$i]['size'] > $maxtaille)
+        echo 'votre fichier '.$_FILES['fichier'.$i]['name'].' d&eacute;passe la taille maximale.<br>';
 
+      else
+      {
+
+        // On récupère l'extension de notre image uniquement
+        $ext = strtolower(substr(strrchr($_FILES['fichier'.$i]['name'], '.'), 1));
+
+        // On vérifie si elle est dans notre tableau d'extensions
+
+        // Si elle n'y est pas
+        if (!in_array($ext,$tabextension))
+          echo "fichier non pris encharge Extension incorrecte";
+
+        // Si elle y est
+        else
+        {
+          // On prépare notre requête permettant de récupérer l'ID de notre annonce (qui est en Auto-Incrémentation)
+          $req= $bdd->prepare("SELECT id_annonce FROM annonces WHERE type=:type AND ville=:ville AND prix=:prix AND auteur=:auteur");
+
+          // On l'exécute
+          $req->execute(array(
+            'type' => $_POST["type"],
+            'ville' => $_POST["ville"],
+            'prix' => $_POST["prix"],
+            'auteur' => $_SESSION["user"]
+          ));
+
+          // On met le résultat dans une variable
+          $donnees=$req->fetch();
+
+          // On récupère notre ID
+          $id = $donnees["id_annonce"];
+
+          // On ferme notre requête
+          $req->CloseCursor();
+
+          // On prépare notre requête d'insertion
+          $req=$bdd->prepare("INSERT INTO image(nom_image,annonce)
+                              VALUES(:nom_image, :annonce)");
+
+          // On l'exécute
+          $req->execute(array(
+            'nom_image' => $_FILES['fichier'.$i]['name'],
+            'annonce' => $id
+          ));
+       }
+      }
+    }
+  }
 
 
   elseif (isset($_POST["type"]) && isset($_POST["ville"]) && isset($_POST["prix"]) && isset($_POST["nombre_images"]))
