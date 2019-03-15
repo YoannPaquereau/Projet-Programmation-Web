8ef076750451609bdda5239dec5b71af0cc6362b
abdoulaziztraore <abdoulaziztraore1997@gmail.com>
2019-03-15 17:15:52 +0000

    .

--- pub_annonces.php
+++ pub_annonces.php
@@ -27,19 +27,53 @@
   $req= $bdd->prepare("INSERT INTO annonces(type,ville,prix,date_publication,auteur)
                            VALUES(:type,:ville,:prix,NOW(),:auteur) ");
 
+
+
   // On exécute la requête avec nos valeurs
   $req->execute(array(
-    'type' => $_POST["type"],
-    'ville' => $_POST["ville"],
-    'prix' => $_POST["prix"],
-    'auteur' => $_SESSION["user"]
+ 'type' => $_POST["type"],
+ 'ville' => $_POST["ville"],
+ 'prix' => $_POST["prix"],
+ 'auteur' => $_SESSION["user"]
+));
 
-  ));
-  echo $_SESSION['user'].' vous avez ajoutez avec succès un fichier de  type '.$_POST['type'].', a  '.$_POST['ville'].' au prix  '.$_POST['prix'].' <br>';
-   echo $_FILES['fichier0']['size'];
+  echo $_SESSION['user'].' vous avez ajoutez avec succès une publication de  type '.$_POST['type'].', a  '.$_POST['ville'].' au prix  '.$_POST['prix'].' <br>';
+  $maxtaille = 10000000;
+  $tabextension=array("png","jpeg","jpg");
+  for($i=0;$i<$_POST['nombre_images'];$i++)
+
+{
+
+  if   ($_FILES['fichier'.$i]['size'] > $maxtaille)
+       echo 'votre fichier '.$_FILES['fichier'.$i]['name'].' d&eacute;passe la taille maximale.<br>';
+  else{
+  $ext = strtolower(  substr(  strrchr($_FILES['fichier'.$i]['name'], '.')  ,1)  );
+     if (!in_array($ext,$tabextension) ) echo "fichier non pris encharge Extension incorrecte";
+     else {
+      $req= $bdd->prepare("SELECT id_annonce FROM annonces WHERE type=:type AND ville=:ville AND prix=:prix AND auteur=:auteur");
+      $req->execute(array(
+     'type' => $_POST["type"],
+     'ville' => $_POST["ville"],
+     'prix' => $_POST["prix"],
+     'auteur' => $_SESSION["user"]
+ ));
+ $donnees=$req->fetch();
+ $id=$donnees["id_annonce"];
+
+
+ $req->CloseCursor();
+    $req=$bdd->prepare("INSERT INTO image(nom_image,annonce)VALUES(:nom_image,:annonce)");
+    $req->execute(array(
+      'nom_image' => $_FILES['fichier'.$i]['name'],
+      'annonce' => $id
+    ));
+     }
+
+    }
+
  }
 
-
+}
 
 
 
